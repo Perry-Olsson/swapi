d21bc4b9f9b5dd93168d92b22dc7c2b2d7aec0ec
Perry Olsson <olsson.perry@gmail.com>
2020-03-29 20:20:06 -0700

    nametag resize fix

--- src/components/Nametag.js
+++ src/components/Nametag.js
@@ -1,55 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Nametag extends Component {
-    constructor({ person }) {
-        super({ person })
-        this.state = {
-            isHovering: false,
-            name: person.name,
-            height: person.height,
-            mass: person.mass,
-            gender: person.gender
-        };
-        this.handleEnter = this.handleEnter.bind(this);
-        this.handleExit = this.handleExit.bind(this);
-    }
+  constructor({ person }) {
+    super({ person });
+    this.state = {
+      isHovering: false,
+      name: person.name,
+      height: person.height,
+      mass: person.mass,
+      gender: person.gender
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleExit = this.handleExit.bind(this);
+  }
 
-    toggleHoverState(state) {
-        return {
-            isHovering: !state.isHovering,
-        };
-    }
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering
+    };
+  }
 
-    handleEnter() {
-        setTimeout(() => {
-            this.setState(this.toggleHoverState)
-        }, 150);
-    }
+  async handleEnter(event) {
+    const hoveredNametag = event.target;
+    const getHeight = () => {
+      return hoveredNametag.offsetHeight;
+    };
+    setTimeout(() => {
+      if (getHeight() > 280) {
+        this.setState(this.toggleHoverState);
+      }
+    }, 150);
 
-    handleExit() { this.setState(this.toggleHoverState); }
+    //}
+  }
 
-    render() {
-        const { name, height, mass, gender } = this.state;
-        return (
-            <div
-                id='nametag'
-                className='flex flex-column justify-center items-center box ba b--yellow white br4 ph4 mv3 f3'
-                onMouseEnter={this.handleEnter}
-                onMouseLeave={this.handleExit}
-            >
-                <h3>{name}</h3>
-                {
-                    this.state.isHovering &&
-                    <div>
-                        <p>height: {height}</p>
-                        <p>mass: {mass}</p>
-                        <p>gender: {gender}</p>
-                    </div>
-                }
-            </div>
-        );
-    }
+  handleExit() {
+    setTimeout(() => {
+      this.setState({ isHovering: false });
+    }, 10);
+  }
+
+  render() {
+    const { name, height, mass, gender } = this.state;
+    return (
+      <div
+        id="nametag"
+        className="hover flex flex-column justify-center items-center box ba b--yellow white br4 ph4 mv3 f3"
+        onMouseEnter={this.handleEnter}
+        onMouseLeave={this.handleExit}
+      >
+        <h3>{name}</h3>
+        {this.state.isHovering && (
+          <div className="hover extendedInfo">
+            <p>height: {height}</p>
+            <p>mass: {mass}</p>
+            <p>gender: {gender}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
-
-export default Nametag
+export default Nametag;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
-import NametagList from '../components/NametagList';
-import './App.css';
+import React, { Component } from "react";
+import NametagList from "../components/NametagList";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       people: []
-    }
+    };
   }
 
   componentDidMount() {
     const getPeople = async () => {
-      const peopleData = await fetch('https://swapi.co/api/people/');
+      const peopleData = await fetch("https://swapi.co/api/people/");
       const peeps = await peopleData.json();
       this.setState({ people: peeps.results });
-    }
+    };
     getPeople();
   }
-  
+
   render() {
     const people = this.state.people;
     return (
-      <div className='tc box'>
-        <h1 className='f-headline ma0 white'><img alt='Star Wars' src='https://lh3.googleusercontent.com/proxy/DkdJG9eRgRZLgFfh-YtFtLbXko2ESI4CEvkVpH65tKa3xCbkztXV4Y4CjghNhAaGDuG6kBZy3PAkYMhcMRYyJNeH3O5J95phuDtdoeJKh5UUnAc-q3uJGfdIrWot' height='300px' /></h1>
+      <div className="tc box">
+        <h1 className="f-headline ma0 white">
+          <img
+            alt="Star Wars"
+            src="http://loodibee.com/wp-content/uploads/Star-Wars-transparent-logo-300x300.png"
+            height="300px"
+          />
+        </h1>
         <div>
-          <NametagList people={people}/>
+          <NametagList people={people} />
         </div>
       </div>
     );
