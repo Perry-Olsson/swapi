642c12d1d05f2adae29a98e5fb872f81f85b5381
Perry Olsson <olsson.perry@gmail.com>
2020-03-29 21:44:24 -0700

    Updates

---
+++ precache-manifest.9cb7c59e69e9e5a3a117dceb0a99f3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240db664e9b6fa62ba5c847fe088fdda",
+    "url": "/git@github.com:Perry-Olsson/swapi.git/index.html"
+  },
+  {
+    "revision": "40e70cbae3b51c4b52d7",
+    "url": "/git@github.com:Perry-Olsson/swapi.git/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "69ac168f1ffe8d035a55",
+    "url": "/git@github.com:Perry-Olsson/swapi.git/static/css/main.897e90da.chunk.css"
+  },
+  {
+    "revision": "40e70cbae3b51c4b52d7",
+    "url": "/git@github.com:Perry-Olsson/swapi.git/static/js/2.99399b2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:Perry-Olsson/swapi.git/static/js/2.99399b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ac168f1ffe8d035a55",
+    "url": "/git@github.com:Perry-Olsson/swapi.git/static/js/main.2d5bd0b8.chunk.js"
+  },
+  {
+    "revision": "7247ed781fd5baf5e49f",
+    "url": "/git@github.com:Perry-Olsson/swapi.git/static/js/runtime-main.7c933202.js"
+  }
+]);
