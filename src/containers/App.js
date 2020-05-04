99e4459a145f7e1ade26bb5ac18530af3d315675
Perry Olsson <olsson.perry@gmail.com>
2020-05-04 15:20:39 -0700

    updated url

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      people: []
+      people: [],
     };
     this.handleExit = this.handleExit.bind(this);
     this.handleStart = this.handleStart.bind(this);
@@ -15,7 +15,7 @@ class App extends Component {
     const getPeople = async () => {
       const getHomeWorld = async () => {
         const people = await Promise.all(
-          this.state.people.map(async person => {
+          this.state.people.map(async (person) => {
             const homeWorldData = await fetch(person.homeworld);
             const homeWorld = await homeWorldData.json();
             person.homeworld = homeWorld.name;
@@ -31,35 +31,35 @@ class App extends Component {
       };
       const getRemainingData = async () => {
         const people = await Promise.all(
-          this.state.people.map(async person => {
+          this.state.people.map(async (person) => {
             const [films, species, vehicles, starships] = [
               await Promise.all(
                 person.films.map(
-                  async film =>
+                  async (film) =>
                     await fetch(film)
-                      .then(r => r.json())
-                      .then(r => r.title)
+                      .then((r) => r.json())
+                      .then((r) => r.title)
                 )
               ),
               await fetch(person.species)
-                .then(result => result.json())
-                .then(result => result.name),
+                .then((result) => result.json())
+                .then((result) => result.name),
               await Promise.all(
                 person.vehicles.map(
-                  async vehicle =>
+                  async (vehicle) =>
                     await fetch(vehicle)
-                      .then(result => result.json())
-                      .then(r => r.name)
+                      .then((result) => result.json())
+                      .then((r) => r.name)
                 )
               ),
               await Promise.all(
                 person.starships.map(
-                  async starship =>
+                  async (starship) =>
                     await fetch(starship)
-                      .then(result => result.json())
-                      .then(r => r.name)
+                      .then((result) => result.json())
+                      .then((r) => r.name)
                 )
-              )
+              ),
             ];
             person.films = films;
             person.species = species;
@@ -70,13 +70,13 @@ class App extends Component {
         );
         this.setState({ people });
       };
-      const peopleData = await fetch("https://swapi.co/api/people/");
+      const peopleData = await fetch("https://swapi.dev/api/people/");
       const peeps = await peopleData.json();
       this.setState({
-        people: peeps.results.map(person => {
+        people: peeps.results.map((person) => {
           person.isHovering = false;
           return person;
-        })
+        }),
       });
       getHomeWorld();
       getRemainingData();
